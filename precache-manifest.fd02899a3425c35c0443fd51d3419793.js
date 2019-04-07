74c3cc64f0c399d72ba9a29fe7adcfc99e73f321
anbut02 <49370690+anbut02@users.noreply.github.com>
2019-04-07 23:27:12 +0530

    Updates

---
+++ precache-manifest.fd02899a3425c35c0443fd51d3419793.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8773c58af7b3e3e390b8",
+    "url": "/react-gh-pages1/static/js/runtime~main.22a9be7e.js"
+  },
+  {
+    "revision": "2a585ef503b197f6ea4e",
+    "url": "/react-gh-pages1/static/js/main.e79b3c53.chunk.js"
+  },
+  {
+    "revision": "5f1b2c7609caef80844e",
+    "url": "/react-gh-pages1/static/js/2.15911f45.chunk.js"
+  },
+  {
+    "revision": "2a585ef503b197f6ea4e",
+    "url": "/react-gh-pages1/static/css/main.c0bff90e.chunk.css"
+  },
+  {
+    "revision": "5f1b2c7609caef80844e",
+    "url": "/react-gh-pages1/static/css/2.13944c2b.chunk.css"
+  },
+  {
+    "revision": "9ed3e863cee414b7c1c4c25a2d6ba475",
+    "url": "/react-gh-pages1/index.html"
+  }
+];
